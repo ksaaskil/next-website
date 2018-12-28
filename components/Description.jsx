13248e874ef581fe694b42d07371048556196d4c
Kimmo Sääskilahti <ksaaskil@gmail.com>
2018-12-28 19:44:15 +0200

    Add background.

--- components/Description.jsx
+++ components/Description.jsx
@@ -4,19 +4,19 @@ import '../css/custom.css'
 import '../css/fx.css'
 import '../css/magnific-popup.css'
 
-const Description = () => (<section id="desc-text-4" className="pt-75 pb-75 pt-md-150 pb-md-150 text-center light spr-edit-el spr-oc-show spr-wout">
-    <div className="container">
+const Description = () => (<section id="desc-text-4" className="pt-75 pb-75 pt-md-150 pb-md-150 text-center dark spr-edit-el spr-oc-show spr-wout">
+    <div className="container" >
         <div className="row">
             <div className="col-lg-6 ml-auto mr-auto">
-                <h3 className="mb-20"><strong>Feature description</strong></h3>
+                <h3 className="mb-20"><strong>Kimmo Sääskilahti Photography</strong></h3>
                 <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 40 20" width="40" className="mb-30 svg-secondary" src="images/gallery/decor/line-h-1.svg" alt="sep"><path d="m0 8h40v4h-40z" fillRule="evenodd"></path></svg>
-                <p className="mb-50">In our work we try to use only the most modern, convenient and interesting solutions. We want the template you downloaded look unique and new for such a long time as it is possible. Our elements have no excessive gloss, but they are always actual.</p>
-                <a className="btn btn-secondary" href="#">Details</a>
+                <p className="mb-50">Love the nature.</p>
+                {/*<a className="btn btn-secondary" href="#">Details</a>*/}
             </div>
         </div>
     </div>
-    <div className="bg-wrap">
-        <div className="bg"></div>
+    <div className="bg-wrap" >
+        <div className="bg" style={{backgroundImage: `url(${require("../images/bear.jpg")})`, backgroundPosition: "center center", backgroundSize: "cover"}}></div>
     </div>
 </section>)
 
