a3155360feb02f12cc7e1fbe800ee49748876da2
Kimmo Sääskilahti <ksaaskil@gmail.com>
2018-12-28 21:41:46 +0200

    Add new images.

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,10 +5,12 @@ import '../css/custom.css'
 import '../css/fx.css'
 import '../css/magnific-popup.css'
 
+const Background = require("../images/thunder.jpg")
+
 const Header = () => (<header id="header-text-2" className="pt-75 pb-75 pt-lg-200 pb-lg-200 dark spr-edit-el spr-oc-show spr-wout">
     <div className="container">
         <div className="row">
-            <div className="col-lg-12">
+            <div className="col-lg-12" align="center">
                 {/*<img className="mb-75" alt="sign" src="images/gallery/logo-3.png" height="120">*/}
                 <h3><strong>Kimmo Sääskilahti Photography</strong></h3>
                 <h4><strong>Love the Nature.</strong></h4>
@@ -16,7 +18,7 @@ const Header = () => (<header id="header-text-2" className="pt-75 pb-75 pt-lg-20
         </div>
     </div>
     <div className="bg-wrap">
-        <div className="bg" style={{backgroundImage: `url(${require("../images/bear.jpg")})`, backgroundPosition: "center center", backgroundSize: "cover"}}></div>
+        <div className="bg" style={{backgroundImage: `url(${Background})`, backgroundPosition: "center center", backgroundSize: "cover"}}></div>
     </div>
     </header>
 )
