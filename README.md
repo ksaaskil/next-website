2914aa0244c94968ffa1ceabbbdc61b8a63b59c2
Kimmo Sääskilahti <ksaaskil@gmail.com>
2018-12-28 19:56:05 +0200

    Add option to export static site.

--- README.md
+++ README.md
@@ -18,3 +18,18 @@ yarn storybook
 ```bash
 yarn build-storybook
 ```
+
+## Build static
+
+```bash
+next build
+next export
+```
+
+Test the static deployment:
+
+```bash
+cd out
+[yarn global add serve]
+serve -p 8080
+```
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 const withCSS = require('@zeit/next-css')
 const withImages = require('next-images')
 
-module.exports = withImages(withCSS())
+module.exports = withImages(withCSS({
+    exportPathMap: function () {
+        return {
+          '/': { page: '/' }
+        }
+      }
+    }
+))
