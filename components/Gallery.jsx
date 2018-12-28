9c96e377de4bbcfc6957e9922f1847b343b24a23
Kimmo Sääskilahti <ksaaskil@gmail.com>
2018-12-28 21:08:30 +0200

    Work on the gallery.

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -5,38 +5,40 @@ import '../css/custom.css'
 import '../css/fx.css'
 import '../css/magnific-popup.css'
 
+const Bear = require('../images/bear.jpg')
+
 const Gallery = () => (<section id="gallery-2col" className="pt-75 pb-75 pt-md-150 pb-md-150 gallery light spr-edit-el spr-oc-show spr-wout">
     <div className="container">
         <div className="row">
-            <div className="col-md">
+            <div className="col-md-6">
                 <div className="gallery-item gallery-style-5 mb-50 dark padding">
-                    <a href="images/gallery/item-nature-1.jpg" className="image-popup">
-                        <img src={require('../images/bear.jpg')} alt="" className="item-img"/>
+                    <a href={Bear} className="image-popup">
+                        <img src={Bear} alt="" className="item-img"/>
                     </a>
-                    <div className="item-title"><p><strong>Gallery item</strong></p></div>
+                    <div className="item-title"><p><strong>Brown bear, Kuusamo</strong></p></div>
                     <div className="item-icon"><svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 0 16 16" width="16" className="icon svg-default" src="images/gallery/icons/plus.svg" alt=""><path d="m9 7h7v2h-7v7h-2v-7h-7v-2h7v-7h2z" fillRule="evenodd"></path></svg></div>
                 </div>
                 <div className="gallery-item gallery-style-5 mb-50 dark padding">
-                    <a href="images/gallery/item-nature-2.jpg" className="image-popup">
-                        <img src={require('../images/bear.jpg')} alt="" className="item-img"/>
+                    <a href={Bear} className="image-popup">
+                        <img src={Bear} alt="" className="item-img"/>
                     </a>
-                    <div className="item-title"><p><strong>Gallery item</strong></p></div>
+                    <div className="item-title"><p><strong>Brown bear, Kuusamo</strong></p></div>
                     <div className="item-icon"><svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 0 16 16" width="16" className="icon svg-default" src="images/gallery/icons/plus.svg" alt=""><path d="m9 7h7v2h-7v7h-2v-7h-7v-2h7v-7h2z" fillRule="evenodd"></path></svg></div>
                 </div>
             </div>
             <div className="col-md">
                 <div className="gallery-item gallery-style-5 mb-50 dark padding">
-                    <a href="images/gallery/item-nature-3.jpg" className="image-popup">
-                        <img src={require('../images/bear.jpg')} alt="" className="item-img"/>
+                    <a href={Bear} className="image-popup">
+                        <img src={Bear} alt="" className="item-img"/>
                     </a>
-                    <div className="item-title"><p><strong>Gallery item</strong></p></div>
+                    <div className="item-title"><p><strong>Brown bear, Kuusamo</strong></p></div>
                     <div className="item-icon"><svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 0 16 16" width="16" className="icon svg-default" src="images/gallery/icons/plus.svg" alt=""><path d="m9 7h7v2h-7v7h-2v-7h-7v-2h7v-7h2z" fillRule="evenodd"></path></svg></div>
                 </div>
                 <div className="gallery-item gallery-style-5 mb-50 dark padding">
-                    <a href="images/gallery/item-nature-4.jpg" className="image-popup">
-                        <img src={require('../images/bear.jpg')} alt="" className="item-img"/>
+                    <a href={Bear} className="image-popup">
+                        <img src={Bear} alt="" className="item-img"/>
                     </a>
-                    <div className="item-title"><p><strong>Gallery item</strong></p></div>
+                    <div className="item-title"><p><strong>Brown bear, Kuusamo</strong></p></div>
                     <div className="item-icon"><svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 0 16 16" width="16" className="icon svg-default" src="images/gallery/icons/plus.svg" alt=""><path d="m9 7h7v2h-7v7h-2v-7h-7v-2h7v-7h2z" fillRule="evenodd"></path></svg></div>
                 </div>
             </div>
