4875a592bc0da793684fdd530218a21b69033b24
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-06-06 21:09:20 +0300

    Change headline.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,10 @@ export default () => (
   <Layout>
     <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
       <h1>Hi!</h1>
-      <p>I'm currently migrating my website to use Gatsby.js.</p>
+      <p>
+        I'm currently migrating my website to use Gatsby.js, please come back
+        later!
+      </p>
     </div>
   </Layout>
 )
