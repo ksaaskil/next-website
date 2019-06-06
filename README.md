b53bdf4e55e5e87aefd34fdf4fbc2190f35070d3
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-06-06 20:48:37 +0300

    Clean storybook.

--- README.md
+++ README.md
@@ -1,21 +1,20 @@
-# [Website with Next.js](https://kimmosaaskilahti.fi)
+# [Website with Gatsby.js](https://kimmosaaskilahti.fi)
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/2813de34-3363-4ca8-8136-e60c0088a8a6/deploy-status)](https://app.netlify.com/sites/jovial-chandrasekhar-6b410b/deploys)
 
 ## Usage
 
-
 ### Run development server
 
 ```bash
 yarn
-yarn dev
+yarn develop
 ```
 
 ### Export static files
 
 ```bash
-yarn export
+yarn build
 ```
 
 ### Run Storybook
@@ -30,10 +29,8 @@ yarn storybook
 yarn build-storybook
 ```
 
-### Test the export
+### Serve the build
 
 ```bash
-[yarn global add serve]
-cd out
-serve -p 8080
+yarn serve
 ```
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,32 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import { linkTo } from '@storybook/addon-links'
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo'
-import Footer from '../components/Footer'
-import Gallery from '../components/Gallery'
-import Header from '../components/Header'
+import { Button, Welcome } from '@storybook/react/demo';
 
-storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
-))
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
-      <span role='img' aria-label='so cool'>
+      <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
     </Button>
-  ))
-
-storiesOf('Gallery', module).add('vanilla', () => <Gallery />)
-
-storiesOf('Header', module).add('vanilla', () => <Header />)
-
-storiesOf('Footer', module).add('vanilla', () => <Footer />)
+  ));
