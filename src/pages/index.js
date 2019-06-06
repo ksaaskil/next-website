1d03e0bc24bfebe9c0286a2ab0be5e7a82f4a875
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-06-06 16:42:50 +0300

    Add simple stylings.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
+import "./index.scss"
 
 export default () => (
-  <div style={{ color: `purple` }}>
-    <Link to="/contact/">Contact</Link>
-    <h1>Hello Gatsby!</h1>
-    <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  <div className="parallax">
+    <div id="stars1" />
+    <div id="stars2" />
+    <div id="stars3" />
   </div>
 )
