e3297e0e98e3066587bb368ba1474dcc9e7f639d
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-06-09 15:08:57 +0300

    Refactor styles, add story.

--- src/__stories__/index.stories.js
+++ src/__stories__/index.stories.js
@@ -1,5 +1,8 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
 import Index from "../pages/index"
+import About from "../pages/about"
 
 storiesOf(`index.js`, module).add(`default`, () => <Index />)
+
+storiesOf(`index.js`, module).add(`about`, () => <About />)
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,3 +1,4 @@
+import "../styles/global.css"
 import React from "react"
 import "./layout.scss"
 import { Link } from "gatsby"
