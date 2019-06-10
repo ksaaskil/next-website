0c0a013ebc211eb4993b156cdb6a38bdbc54a909
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-06-10 21:52:24 +0300

    Add two more pics.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,16 @@ import "./index.scss"
 import Layout from "../components/layout"
 import Image from "../components/image"
 
+const Peacock = "IMG_7126-Edit-2.jpg"
+const BumbleBee = "IMG_6869-Edit-2.jpg"
+const KuusamoBear = "20180821-IMG_9232.jpg"
+const Suomujoki = "20180829-IMG_9945-Edit-Edit.jpg"
+
 export default () => (
   <Layout>
-    <Image image={"20180821-IMG_9232.jpg"} caption={"Kuusamo, Finland"} />
-    <Image
-      image={"20180829-IMG_9945-Edit-Edit.jpg"}
-      caption={"Suomujoki, Finland"}
-    />
+    <Image image={KuusamoBear} caption={"Kuusamo, Finland"} />
+    <Image image={Suomujoki} caption={"Suomujoki, Finland"} />
+    <Image image={BumbleBee} caption={"Hauho, Finland"} />
+    <Image image={Peacock} caption={"Hauho, Finland"} />
   </Layout>
 )
