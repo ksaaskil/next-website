5b2aab5e71cefb2ff5b042bc507a86d454737913
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-06-18 15:07:40 +0300

    Add social icons. (#3)
    * Add social icons.
    
    * Clean-up.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
   plugins: [
     "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
     "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-plugin-gtag`,
