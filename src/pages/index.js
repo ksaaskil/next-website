42e3517c107bcb2388e640ea829587236223aff3
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-06-24 09:50:59 +0300

    Add simple A/B testing (#6)
    * Add a dummy a/b test.
    
    * Consider scrolling to bottom a win.
    
    * Clean-up.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,10 @@
 import React from "react"
+import {
+  Experiment,
+  Variant,
+  emitter,
+  experimentDebugger,
+} from "@marvelapp/react-ab-test"
 import "./index.scss"
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,11 +14,78 @@ const BumbleBee = "IMG_6869-Edit-2.jpg"
 const KuusamoBear = "20180821-IMG_9232.jpg"
 const Suomujoki = "20180829-IMG_9945-Edit-Edit.jpg"
 
-export default () => (
-  <Layout>
+const Images1 = () => (
+  <>
     <Image image={KuusamoBear} caption={"Kuusamo, Finland"} />
     <Image image={Suomujoki} caption={"Suomujoki, Finland"} />
     <Image image={BumbleBee} caption={"Hauho, Finland"} />
     <Image image={Peacock} caption={"Hauho, Finland"} />
-  </Layout>
+  </>
 )
+
+const Images2 = () => (
+  <>
+    <Image image={Peacock} caption={"Hauho, Finland"} />
+    <Image image={KuusamoBear} caption={"Kuusamo, Finland"} />
+    <Image image={Suomujoki} caption={"Suomujoki, Finland"} />
+    <Image image={BumbleBee} caption={"Hauho, Finland"} />
+  </>
+)
+
+const IMAGE_EXPERIMENT_NAME = "Image experiment"
+
+const Index = () => (
+  <>
+    <Experiment name={IMAGE_EXPERIMENT_NAME}>
+      <Variant name="A">
+        <Images1 />
+      </Variant>
+      <Variant name="B">
+        <Images2 />
+      </Variant>
+    </Experiment>
+  </>
+)
+
+experimentDebugger.enable()
+
+export default () => {
+  React.useEffect(() => {
+    let emittedWin = false
+
+    const scrollListener = e => {
+      const element = e.target.scrollingElement
+      const isAtBottom =
+        element.scrollHeight - element.scrollTop === element.clientHeight
+      if (isAtBottom && !emittedWin) {
+        emittedWin = true
+        emitter.emitWin(IMAGE_EXPERIMENT_NAME)
+      }
+    }
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", scrollListener)
+    }
+
+    emitter.addPlayListener((experimentName, variantName) => {
+      console.log(
+        `Displaying experiment ${experimentName} variant ${variantName}`
+      )
+    })
+
+    emitter.addWinListener((experimentName, variantName) => {
+      console.log(`Variant ${variantName} of experiment ${experimentName} won`)
+    })
+
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("scroll", scrollListener)
+      }
+    }
+  }, [])
+
+  return (
+    <Layout>
+      <Index />
+    </Layout>
+  )
+}
