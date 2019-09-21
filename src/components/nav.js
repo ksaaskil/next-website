1db899922c1da3f2c09a6c2e56bb63842176e429
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-21 18:29:25 +0300

    Add nav. (#13)
    * Start working on nav.
    
    * Hide all nav for now.
    
    * Add birds button.
    
    * Use OutboundLink.

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,17 +1,19 @@
 import React from "react"
+import "./nav.scss"
 import { OutboundLink } from "gatsby-plugin-google-gtag"
-import styles from "../styles/header.module.css"
 import InstagramIcon from "../assets/instagram.logo.svg"
 import GitHubIcon from "../assets/github.logo.svg"
 import ScholarIcon from "../assets/scholar.logo.svg"
 
-const Header = props => (
-  <div className={styles.header}>
-    <div className={styles.headerName}>
-      <h1 className={styles.headerNameH1}>Kimmo Sääskilahti Photography</h1>
-    </div>
-    <div className={styles.headerIcons}>
-      <OutboundLink href="https://www.instagram.com/saaskimmo/">
+const Nav = () => (
+  <nav className="nav">
+    <div className="nav__items">
+      <div className="nav__item">
+        <OutboundLink href="https://www.birds.cornell.edu/home/bring-birds-back/">
+          Bring birds back
+        </OutboundLink>
+      </div>
+      {/*<OutboundLink href="https://www.instagram.com/saaskimmo/">
         <InstagramIcon />
       </OutboundLink>
       <OutboundLink href="https://github.com/ksaaskil">
@@ -19,9 +21,9 @@ const Header = props => (
       </OutboundLink>
       <OutboundLink href="https://scholar.google.fi/citations?user=r67e0OEAAAAJ&hl=en">
         <ScholarIcon />
-      </OutboundLink>
+</OutboundLink>*/}
     </div>
-  </div>
+  </nav>
 )
 
-export default Header
+export default Nav
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,21 @@
 import React from "react"
 import "./index.scss"
 import Layout from "../components/layout"
+import Nav from "../components/nav"
 
 export const Index = () => {
   return (
-    <header className="header">
-      <div className="header__text-box">
-        <h1 className="heading-primary">
-          <span className="heading-primary--main">Planet Earth</span>
-          <span className="heading-primary--sub">is amazing</span>
-        </h1>
-      </div>
-    </header>
+    <>
+      <header className="header">
+        <div className="header__text-box">
+          <h1 className="heading-primary">
+            <span className="heading-primary--main">Planet Earth</span>
+            <span className="heading-primary--sub">is amazing</span>
+          </h1>
+        </div>
+      </header>
+      <Nav />
+    </>
   )
 }
 
