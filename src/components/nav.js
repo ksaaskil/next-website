5d05c2cfa2de4906b55091d2b4ccda9660d89c4c
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-29 17:31:35 +0300

    Add nav buttons. (#14)
    * Add nav buttons.
    
    * Remove px height.
    
    * Add dev.to button.

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,24 +4,34 @@ import { OutboundLink } from "gatsby-plugin-google-gtag"
 import InstagramIcon from "../assets/instagram.logo.svg"
 import GitHubIcon from "../assets/github.logo.svg"
 import ScholarIcon from "../assets/scholar.logo.svg"
+import DevToIcon from "../assets/devto.logo.svg"
 
 const Nav = () => (
   <nav className="nav">
     <div className="nav__items">
       <div className="nav__item">
-        <OutboundLink href="https://www.birds.cornell.edu/home/bring-birds-back/">
-          Bring birds back
+        <div className="nav__item--birds">
+          <OutboundLink href="https://www.birds.cornell.edu/home/bring-birds-back/">
+            Bring birds back
+          </OutboundLink>
+        </div>
+      </div>
+
+      <div className="header-icons">
+        <OutboundLink href="https://www.instagram.com/saaskimmo/">
+          <InstagramIcon />
+        </OutboundLink>
+        <OutboundLink href="https://github.com/ksaaskil">
+          <GitHubIcon />
+        </OutboundLink>
+        <OutboundLink href="https://scholar.google.fi/citations?user=r67e0OEAAAAJ&hl=en">
+          <ScholarIcon />
+        </OutboundLink>
+        <OutboundLink href="https://dev.to/ksaaskil">
+          <DevToIcon />
         </OutboundLink>
       </div>
-      {/*<OutboundLink href="https://www.instagram.com/saaskimmo/">
-        <InstagramIcon />
-      </OutboundLink>
-      <OutboundLink href="https://github.com/ksaaskil">
-        <GitHubIcon />
-      </OutboundLink>
-      <OutboundLink href="https://scholar.google.fi/citations?user=r67e0OEAAAAJ&hl=en">
-        <ScholarIcon />
-</OutboundLink>*/}
+      {/**/}
     </div>
   </nav>
 )
