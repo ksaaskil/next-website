3d1a35e2c8389663bd022f0f98e949af82ef7556
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-04-21 14:37:35 +0300

    Add chakra-ui.

---
+++ gatsby-browser.js
@@ -0,0 +1,18 @@
+import React from "react"
+// import Layout from "./src/components/templates/layout"
+import { ThemeProvider, CSSReset } from "@chakra-ui/core"
+import customTheme from "./theme/theme"
+
+export const wrapPageElement = ({ element, props }) => {
+  // return <Layout {...props}>{element}</Layout>
+  return element
+}
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CSSReset />
+      {element}
+    </ThemeProvider>
+  )
+}
