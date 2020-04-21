9679906182ad59d5233e38361c3ffbcd7669d366
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-04-21 15:46:53 +0300

    Under construction.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,7 @@ export const Index = () => {
         pageTitle="Kimmo Sääskilahti's homepage"
         pageDescription="My blog and stuff"
       />
-      <header className="header">
-        <div className="header__text-box">
-          <h1 className="heading-primary">
-            <span className="heading-primary--main">Under construction</span>
-            <span className="heading-primary--sub">this page is</span>
-          </h1>
-        </div>
-      </header>
+      Under construction this page is.
     </>
   )
 }
