dd570d6d0664dc6923d56e833614e51e4b1b06c2
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2021-01-01 20:16:01 +0200

    Work on bookshelf.

--- src/pages/bookshelf.js
+++ src/pages/bookshelf.js
@@ -1,40 +1,102 @@
 import React from "react"
-import "./bookshelf.scss"
-// import { graphql } from 'gatsby'
 import SEO from "../components/seo"
 import SingleSection from "../components/single-section"
-// import BlogIndex from "../components/blog-list"
 import { Heading } from "@chakra-ui/core"
-import { Text } from "@chakra-ui/core";
+import { Box, Stack, Text, Flex } from "@chakra-ui/core";
+import Img from "gatsby-image"
 
-const Book = ({title, author, description}) => {
-  return (<>
-    <Text fontSize="xl">{title}</Text>
-    <Text as="i">{author}</Text>
-    <Text>{description}</Text>
-    </>
+
+const Book = ({title, author, description, imgData }) => {
+  return (<Flex p="6">
+    <Flex width="250px">
+      {!!imgData ? <Img fixed={imgData.childImageSharp.fixed} /> : null}
+    </Flex>
+    <Box>
+      <Text as="h3" fontWeight="bold" fontSize="xl">{title}</Text>
+      <Text as="p"><Text as="i">{author}</Text></Text>
+      <Text>{description}</Text>
+    </Box>
+    </Flex>
   )
 }
 
-const BookshelfHeader = () => {
-  return (<Heading>Something</Heading>)
+const BookList = ({books, title}) => {
+  return (
+    <Box p="6">
+      <Text fontSize="3xl" fontWeight="bold">{title}</Text>
+    <Stack spacing="48px">
+      {books.map(book => 
+        <Book key={book.title} title={book.title} author={book.author}
+        description={book.description} imgData={book.imgData} />)}
+      </Stack>
+    </Box>)
 }
 
-const books = [
-  {
-    title: "The Power of Habit",
-    author: "Charles Duhigg",
-    description: "Nice book"
-  }
-]
+export default ({ data }) => {
+  const nonTechBooks = [
+    {
+      title: "How to Win Friends and Influence People",
+      author: "Dale Carnegie",
+      cover: "how-to-influence"
+    },
+    {
+      title: "The Lean Startup",
+      author: "Eric Ries",
+      cover: "how-to-influence"
+    },
+    {
+      title: "The Clean Coder",
+      author: "Robert C. Martin",
+      cover: "how-to-influence"
+    },
+    {
+      title: "Concise Laws of Human Nature",
+      author: "Robert Greene",
+      cover: "how-to-influence"
+    }
+  ]
+  
+  const techBooks = [
+    {
+      title: "Designing Data-Intensive Applications",
+      author: "Martin Kleppman",
+      cover: "how-to-influence"
+    },
+    {
+      title: "The Pragmatic Programmer: Your Journey to Mastery, 20th Anniversary Edition",
+      author: "Andy Hunt, Dave Thomas",
+      cover: "how-to-influence"
+    },
+    {
+      title: "Functional Programming in Scala",
+      author: "Paul Chiusano and Rúnar Bjarnason",
+      cover: "how-to-influence"
+    } ,
+    {
+      title: "Clean Code",
+      author: "Robert C. Martin",
+      cover: "how-to-influence"
+    }
+  ]
+  const allImages = data.allImages.edges.map(e => e.node);
 
-const BookList = () => {
-  return (<>{books.map(book => 
-    <Book key={book.title} title={book.title} author={book.author}
-    description={book.description} />)}</>);
-}
+  const nonTechBooksWithImgData = nonTechBooks.map(book => {
+    const maybeImg = allImages.filter(e => e.name == book.cover)
+    console.log(maybeImg)
+    return {
+      ...book,
+      imgData: maybeImg.length > 0 ? maybeImg[0] : null
+    }
+  })
 
-export default ({ props }) => {
+  const techBooksWithImgData = techBooks.map(book => {
+    const maybeImg = allImages.filter(e => e.name == book.cover)
+    return {
+      ...book,
+      imgData: maybeImg ? maybeImg[0] : null
+    }
+  })
+
   return (
     <>
       <SEO
@@ -42,8 +104,46 @@ export default ({ props }) => {
         pageDescription="My favorite books for developers"
       />
       <SingleSection heading="Kimmo's bookshelf">
-        <BookList />
+        <BookList title="Non-technical books" books={nonTechBooksWithImgData} />
+        <BookList title="Technical books" books={techBooksWithImgData} />
       </SingleSection>
     </>
   )
 }
+
+export const query = graphql`
+  query covers {
+    howToInfluence: file(relativePath: { eq: "how-to-influence.jpg" }) {
+      childImageSharp {
+        fixed(width:150) {
+          ...GatsbyImageSharpFixed
+        }
+        fluid(maxWidth:250) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    allImages: allFile(filter: {
+        extension: { regex: "/(jpg)|(png)|(jpeg)/" }
+      }) {
+      edges {
+        node {
+          base
+          name
+          childImageSharp {
+            fixed(width:150) {
+              ...GatsbyImageSharpFixed
+            }
+            fluid(maxWidth:250) {
+              base64
+              src
+              srcSet
+              aspectRatio
+              sizes
+            }
+          }
+        }
+      }
+    }
+  }
+`
